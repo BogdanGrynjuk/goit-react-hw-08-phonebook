e82f7867f564e2e283cb811d1b56d87c92246025
BogdanGrynjuk <bgrynjuk@ukr.net>
2024-05-12 13:31:36 +0300

    clones goit-react-hw-08-phonebook

--- src/redux/contacts/selectors.js
+++ src/redux/contacts/selectors.js
@@ -1,6 +1,9 @@
-import { createSelector } from "@reduxjs/toolkit";
+import { createSelector } from '@reduxjs/toolkit';
 
-export const selectContacts = state => state.contacts.items;
+export const selectContacts = state => {
+  console.log(state.contacts);
+  return state.contacts.items;
+};
 export const selectIsLoading = state => state.contacts.isLoading;
 export const selectError = state => state.contacts.error;
 export const selectFilter = state => state.filter;
@@ -8,10 +11,14 @@ export const selectFilter = state => state.filter;
 export const selectVisibleContacts = createSelector(
   [selectContacts, selectFilter, selectIsLoading],
   (contacts, filter, isLoading) => {
-    if (isLoading) { return [] };  
+    if (isLoading) {
+      return [];
+    }
     if (filter) {
-      return contacts.filter(contact => contact.name.toLowerCase().includes(filter.toLowerCase()));
-    };    
+      return contacts.filter(contact =>
+        contact.name.toLowerCase().includes(filter.toLowerCase())
+      );
+    }
     return contacts;
   }
 );
